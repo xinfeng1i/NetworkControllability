fe458cb2066cf26b52374f3319c93fe1e3ca55d3
python27 <silfer.lee@gmail.com>
2015-05-20 22:38:14 +0800

    test file

--- NetworkControllability/test.py
+++ NetworkControllability/test.py
@@ -1,10 +1,15 @@
 import numpy as np
 import networkx as nx
-import exact_controllability
+import exact_controllability as ECT
 import matplotlib.pyplot as plt
 import matplotlib.pylab as plb
 import csv
 import operator
+import random
+import os
+import subprocess
+import threading
+
 
 def exact_controllability_run_on_ER_one_time(n, prob_start, prob_end, data_cnt):
     probs = np.linspace(prob_start, prob_end, num = data_cnt);
@@ -38,22 +43,57 @@ def exact_controllability_run_on_ER(n, prob_start, prob_end, data_cnt, runcnt):
     plt.ylabel('$n_D$')
     plt.show()
 
-class A():
-    def __init__(self):
-        self.x = 10
-        self.y = 20
-class B():
-    def __init__(self):
-        self.a = 30
-        self.b = 40
-        self.A = A()
 
 if __name__ == "__main__":
-    d = {1:0, 2:5, 3:100, 6:3, 100:3}
+    #G = nx.erdos_renyi_graph(50, 0.99)
+    G = nx.barabasi_albert_graph(1000, 2)
+    #G = nx.watts_strogatz_graph(50, 4, 0.3)
+    #G = nx.newman_watts_strogatz_graph(100, 6, 0.5)
+    print nx.info(G)
+    ND, ND_lambda = ECT.get_number_of_driver_nodes(G)
+    print "ND = ", ND
+    print "ND lambda:", ND_lambda
+    ND, driverNodes = ECT.get_driver_nodes(G)
+    print "ND =", ND
+    #print "driver nodes", driverNodes
 
-    min_value = min([x for x in d.values() if x - 0.0 > 1E-8])
-    min_key = None
-    for k, v in d.iteritems():
-        if v == min_value:
-            min_key = k
-            break
+    degrees = []
+    betweenness = []
+    closeness = []
+    eigenvectorcentrality = []
+
+    tot_degree = nx.degree_centrality(G)
+    tot_betweenness = nx.betweenness_centrality(G,weight=None)
+    #tot_closeness = nx.closeness_centrality(G)
+
+    #tot_eigenvector = nx.eigenvector_centrality(G, max_iter=1000, weight=None)
+    for node in driverNodes:
+        degrees.append(tot_degree[node])
+        betweenness.append(tot_betweenness[node])
+        #closeness.append(tot_closeness[node])
+        #eigenvectorcentrality.append(tot_eigenvector[node])
+
+    #print degrees
+    #print betweenness
+    #print closeness
+    #print eigenvectorcentrality
+    with open("results/degree.txt", "w") as f:
+        for x in degrees:
+            print >> f, x
+    with open("results/betweenness.txt", "w") as f:
+        for x in betweenness:
+            print >> f, x
+    #with open("results/closeness.txt", "w") as f:
+    #    for x in closeness:
+    #        print >> f, x
+    #with open("results/eigenvectorcentrality.txt", "w") as f:
+    #    for x in eigenvectorcentrality:
+    #        print >> f, x
+    #print tot_degree
+    with open("results/tot_degree.txt", "w") as f:
+        for key, value in tot_degree.iteritems():
+            print >> f, value
+
+    with open("results/tot_betweenness.txt", "w") as f:
+        for key, value in tot_betweenness.iteritems():
+            print >> f, value
