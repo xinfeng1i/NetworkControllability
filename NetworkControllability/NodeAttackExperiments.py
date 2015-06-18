790ae58a425a2c9315d71a12bbd59cc14869d63b
python27 <silfer.lee@gmail.com>
2015-06-18 11:13:14 +0800

    robustness test based on node-attack

--- NetworkControllability/NodeAttackExperiments.py
+++ NetworkControllability/NodeAttackExperiments.py
@@ -129,8 +129,8 @@ def NodeAttackonSW():
     k = 4
     fraction = 0.2
     N_rm = int(n * fraction)
-    p = 0.02
-    run_cnt = 30
+    p = 0.1
+    run_cnt = 100
 
     #******** Run Node Attack 1 ********#
     tot_ND1 = [0] * (N_rm + 1)
@@ -139,7 +139,7 @@ def NodeAttackonSW():
         G1 = nx.watts_strogatz_graph(n, k, p)
         print ">>>>>>>>>>>>>>> Random Attack run time count: ", i + 1, "<<<<<<<<<<<<<<<<<<"
         print "graph info", nx.info(G1)
-        ND1, T1, giant1 = RandomNodeAttack(G1, remove_fraction=fraction)
+        ND1, T1 = RandomNodeAttack(G1, remove_fraction=fraction)
         tot_ND1 = [x + y for x, y in zip(tot_ND1, ND1)]
 
     tot_ND1 = [((x + 0.0) / run_cnt) for x in tot_ND1]
@@ -237,8 +237,8 @@ def NodeAttackonNW():
     fraction = 0.2
     N_rm = int(n * fraction)
     k = 4
-    p = 0.02
-    run_cnt = 30
+    p = 0.1
+    run_cnt = 100
 
     #******** Run Node Attack 1 ********#
     tot_ND1 = [0] * (N_rm + 1)
@@ -247,7 +247,7 @@ def NodeAttackonNW():
         G1 = nx.newman_watts_strogatz_graph(n, k, p)
         print ">>>>>>>>>>>>>>> Random Attack run time count: ", i + 1, "<<<<<<<<<<<<<<<<<<"
         print "graph info", nx.info(G1)
-        ND1, T1, giant1 = RandomNodeAttack(G1, remove_fraction=fraction)
+        ND1, T1 = RandomNodeAttack(G1, remove_fraction=fraction)
         tot_ND1 = [x + y for x, y in zip(tot_ND1, ND1)]
 
     tot_ND1 = [((x + 0.0) / run_cnt) for x in tot_ND1]
@@ -445,8 +445,238 @@ def NodeAttackonBA():
 
     print "--- cost time %s seconds ---" %(time.time() - start_time)
 
+def NodeAttackonUSAir():
+    start_time = time.time()
+  
+    n = 332
+    fraction = 0.2
+    N_rm = int(n * fraction)
+    run_cnt = 50
+
+    #******** Run Node Attack 1 ********#
+    tot_ND1 = [0] * (N_rm + 1)
+    tot_T1 = [0] * (N_rm + 1)
+    for i in range(run_cnt):
+        G1 = nx.read_pajek("dataset/USAir97.net")
+        print ">>>>>>>>>>>>>>> Random Attack run time count: ", i + 1, "<<<<<<<<<<<<<<<<<<"
+        print "graph info", nx.info(G1)
+        ND1, T1 = RandomNodeAttack(G1, remove_fraction=fraction)
+        tot_ND1 = [x + y for x, y in zip(tot_ND1, ND1)]
+
+    tot_ND1 = [((x + 0.0) / run_cnt) for x in tot_ND1]
+    tot_T1 = T1
+    tot_ND1 = [(x + 0.0) / (n + 0.0) for x in tot_ND1]
+    tot_T1 = [(x + 0.0) / (n + 0.0) for x in tot_T1]
+
+    with open("results/node_attack1_USAir.csv", "w") as f:
+        writer = csv.writer(f, delimiter=',')
+        writer.writerows(zip(tot_T1, tot_ND1))
+
+    run_cnt = 1
+    #******** Run Node Attack 2 ********#
+    tot_ND1 = [0] * (N_rm + 1)
+    tot_T1 = [0] * (N_rm + 1)
+    for i in range(run_cnt):
+        G1 = nx.read_pajek("dataset/USAir97.net")
+        print ">>>>>>>>>>>>>>> Initial Degree Attack run time count: ", i + 1, "<<<<<<<<<<<<<<<<<<"
+        print "graph info", nx.info(G1)
+        ND1, T1 = InitialNodeDegreeAttack(G1, remove_fraction=fraction)
+        tot_ND1 = [x + y for x, y in zip(tot_ND1, ND1)]
+
+    tot_ND1 = [((x + 0.0) / run_cnt) for x in tot_ND1]
+    tot_T1 = T1
+    tot_ND1 = [(x + 0.0) / (n + 0.0) for x in tot_ND1]
+    tot_T1 = [(x + 0.0) / (n + 0.0) for x in tot_T1]
+
+    with open("results/node_attack2_USAir.csv", "w") as f:
+        writer = csv.writer(f, delimiter=',')
+        writer.writerows(zip(tot_T1, tot_ND1))
+
+    #******** Run Node Attack 3 ********#
+    tot_ND1 = [0] * (N_rm + 1)
+    tot_T1 = [0] * (N_rm + 1)
+    for i in range(run_cnt):
+        G1 = nx.read_pajek("dataset/USAir97.net")
+        print ">>>>>>>>>>>>>>> Recalculated Degree Attack run time count: ", i + 1, "<<<<<<<<<<<<<<<<<<"
+        print "graph info", nx.info(G1)
+        ND1, T1 = RecalculatedNodeDegreeAttack(G1, remove_fraction=fraction)
+        tot_ND1 = [x + y for x, y in zip(tot_ND1, ND1)]
+
+    tot_ND1 = [((x + 0.0) / run_cnt) for x in tot_ND1]
+    tot_T1 = T1
+    tot_ND1 = [(x + 0.0) / (n + 0.0) for x in tot_ND1]
+    tot_T1 = [(x + 0.0) / (n + 0.0) for x in tot_T1]
+
+    with open("results/node_attack3_USAir.csv", "w") as f:
+        writer = csv.writer(f, delimiter=',')
+        writer.writerows(zip(tot_T1, tot_ND1))
+
+
+    #******** Run Node Attack 4 ********#
+    tot_ND1 = [0] * (N_rm + 1)
+    tot_T1 = [0] * (N_rm + 1)
+    for i in range(run_cnt):
+        G1 = nx.read_pajek("dataset/USAir97.net")
+        print ">>>>>>>>>>>>>>> Initial Betweenness Attack run time count: ", i + 1, "<<<<<<<<<<<<<<<<<<"
+        print "graph info", nx.info(G1)
+        ND1, T1 = InitialNodeBetweennessAttack(G1, remove_fraction=fraction)
+        tot_ND1 = [x + y for x, y in zip(tot_ND1, ND1)]
+
+    tot_ND1 = [((x + 0.0) / run_cnt) for x in tot_ND1]
+    tot_T1 = T1
+    tot_ND1 = [(x + 0.0) / (n + 0.0) for x in tot_ND1]
+    tot_T1 = [(x + 0.0) / (n + 0.0) for x in tot_T1]
+
+    with open("results/node_attack4_USAir.csv", "w") as f:
+        writer = csv.writer(f, delimiter=',')
+        writer.writerows(zip(tot_T1, tot_ND1))
+
+    #******** Run Node Attack 5 ********#
+    tot_ND1 = [0] * (N_rm + 1)
+    tot_T1 = [0] * (N_rm + 1)
+    for i in range(run_cnt):
+        G1 = nx.read_pajek("dataset/USAir97.net")
+        print ">>>>>>>>>>>>>>> Recalculated Betweenness Attack run time count: ", i + 1, "<<<<<<<<<<<<<<<<<<"
+        print "graph info", nx.info(G1)
+        ND1, T1 = RecalculatedNodeBetweennessAttack(G1, remove_fraction=fraction)
+        tot_ND1 = [x + y for x, y in zip(tot_ND1, ND1)]
+
+    tot_ND1 = [((x + 0.0) / run_cnt) for x in tot_ND1]
+    tot_T1 = T1
+    tot_ND1 = [(x + 0.0) / (n + 0.0) for x in tot_ND1]
+    tot_T1 = [(x + 0.0) / (n + 0.0) for x in tot_T1]
+
+    with open("results/node_attack5_USAir.csv", "w") as f:
+        writer = csv.writer(f, delimiter=',')
+        writer.writerows(zip(tot_T1, tot_ND1))
+
+    print "--- cost time %s seconds ---" %(time.time() - start_time)
+
+def NodeAttackonErdosNetwork():
+    start_time = time.time()
+  
+    n = 390
+    fraction = 0.2
+    N_rm = int(n * fraction)
+    run_cnt = 50
+
+    #******** Run Node Attack 1 ********#
+    tot_ND1 = [0] * (N_rm + 1)
+    tot_T1 = [0] * (N_rm + 1)
+    for i in range(run_cnt):
+        G1 = nx.read_pajek("dataset/Erdos971.net")
+        isolated_nodes = nx.isolates(G1)
+        G1.remove_nodes_from(isolated_nodes)
+
+        print ">>>>>>>>>>>>>>> Random Attack run time count: ", i + 1, "<<<<<<<<<<<<<<<<<<"
+        print "graph info", nx.info(G1)
+        ND1, T1 = RandomNodeAttack(G1, remove_fraction=fraction)
+        tot_ND1 = [x + y for x, y in zip(tot_ND1, ND1)]
+
+    tot_ND1 = [((x + 0.0) / run_cnt) for x in tot_ND1]
+    tot_T1 = T1
+    tot_ND1 = [(x + 0.0) / (n + 0.0) for x in tot_ND1]
+    tot_T1 = [(x + 0.0) / (n + 0.0) for x in tot_T1]
+
+    with open("results/node_attack1_ErdosNetwork.csv", "w") as f:
+        writer = csv.writer(f, delimiter=',')
+        writer.writerows(zip(tot_T1, tot_ND1))
+
+    run_cnt = 1
+    #******** Run Node Attack 2 ********#
+    tot_ND1 = [0] * (N_rm + 1)
+    tot_T1 = [0] * (N_rm + 1)
+    for i in range(run_cnt):
+        G1 = nx.read_pajek("dataset/Erdos971.net")
+        isolated_nodes = nx.isolates(G1)
+        G1.remove_nodes_from(isolated_nodes)
+
+        print ">>>>>>>>>>>>>>> Initial Degree Attack run time count: ", i + 1, "<<<<<<<<<<<<<<<<<<"
+        print "graph info", nx.info(G1)
+        ND1, T1 = InitialNodeDegreeAttack(G1, remove_fraction=fraction)
+        tot_ND1 = [x + y for x, y in zip(tot_ND1, ND1)]
+
+    tot_ND1 = [((x + 0.0) / run_cnt) for x in tot_ND1]
+    tot_T1 = T1
+    tot_ND1 = [(x + 0.0) / (n + 0.0) for x in tot_ND1]
+    tot_T1 = [(x + 0.0) / (n + 0.0) for x in tot_T1]
+
+    with open("results/node_attack2_ErdosNetwork.csv", "w") as f:
+        writer = csv.writer(f, delimiter=',')
+        writer.writerows(zip(tot_T1, tot_ND1))
+
+    #******** Run Node Attack 3 ********#
+    tot_ND1 = [0] * (N_rm + 1)
+    tot_T1 = [0] * (N_rm + 1)
+    for i in range(run_cnt):
+        G1 = nx.read_pajek("dataset/Erdos971.net")
+        isolated_nodes = nx.isolates(G1)
+        G1.remove_nodes_from(isolated_nodes)
+
+        print ">>>>>>>>>>>>>>> Recalculated Degree Attack run time count: ", i + 1, "<<<<<<<<<<<<<<<<<<"
+        print "graph info", nx.info(G1)
+        ND1, T1 = RecalculatedNodeDegreeAttack(G1, remove_fraction=fraction)
+        tot_ND1 = [x + y for x, y in zip(tot_ND1, ND1)]
+
+    tot_ND1 = [((x + 0.0) / run_cnt) for x in tot_ND1]
+    tot_T1 = T1
+    tot_ND1 = [(x + 0.0) / (n + 0.0) for x in tot_ND1]
+    tot_T1 = [(x + 0.0) / (n + 0.0) for x in tot_T1]
+
+    with open("results/node_attack3_ErdosNetwork.csv", "w") as f:
+        writer = csv.writer(f, delimiter=',')
+        writer.writerows(zip(tot_T1, tot_ND1))
+
+
+    #******** Run Node Attack 4 ********#
+    tot_ND1 = [0] * (N_rm + 1)
+    tot_T1 = [0] * (N_rm + 1)
+    for i in range(run_cnt):
+        G1 = nx.read_pajek("dataset/Erdos971.net")
+        isolated_nodes = nx.isolates(G1)
+        G1.remove_nodes_from(isolated_nodes)
+
+        print ">>>>>>>>>>>>>>> Initial Betweenness Attack run time count: ", i + 1, "<<<<<<<<<<<<<<<<<<"
+        print "graph info", nx.info(G1)
+        ND1, T1 = InitialNodeBetweennessAttack(G1, remove_fraction=fraction)
+        tot_ND1 = [x + y for x, y in zip(tot_ND1, ND1)]
+
+    tot_ND1 = [((x + 0.0) / run_cnt) for x in tot_ND1]
+    tot_T1 = T1
+    tot_ND1 = [(x + 0.0) / (n + 0.0) for x in tot_ND1]
+    tot_T1 = [(x + 0.0) / (n + 0.0) for x in tot_T1]
+
+    with open("results/node_attack4_ErdosNetwork.csv", "w") as f:
+        writer = csv.writer(f, delimiter=',')
+        writer.writerows(zip(tot_T1, tot_ND1))
+
+    #******** Run Node Attack 5 ********#
+    tot_ND1 = [0] * (N_rm + 1)
+    tot_T1 = [0] * (N_rm + 1)
+    for i in range(run_cnt):
+        G1 = nx.read_pajek("dataset/Erdos971.net")
+        isolated_nodes = nx.isolates(G1)
+        G1.remove_nodes_from(isolated_nodes)
+
+        print ">>>>>>>>>>>>>>> Recalculated Betweenness Attack run time count: ", i + 1, "<<<<<<<<<<<<<<<<<<"
+        print "graph info", nx.info(G1)
+        ND1, T1 = RecalculatedNodeBetweennessAttack(G1, remove_fraction=fraction)
+        tot_ND1 = [x + y for x, y in zip(tot_ND1, ND1)]
+
+    tot_ND1 = [((x + 0.0) / run_cnt) for x in tot_ND1]
+    tot_T1 = T1
+    tot_ND1 = [(x + 0.0) / (n + 0.0) for x in tot_ND1]
+    tot_T1 = [(x + 0.0) / (n + 0.0) for x in tot_T1]
+
+    with open("results/node_attack5_ErdosNetwork.csv", "w") as f:
+        writer = csv.writer(f, delimiter=',')
+        writer.writerows(zip(tot_T1, tot_ND1))
+
+    print "--- cost time %s seconds ---" %(time.time() - start_time)
+
+
 if __name__ == "__main__":
-    NodeAttackonER()
     NodeAttackonSW()
     NodeAttackonNW()
-    NodeAttackonBA() 
+    #NodeAttackonUSAir()
+    #NodeAttackonErdosNetwork()
