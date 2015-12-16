c8a4e12030b1aaf6c7339a9a72df39f878b5bea0
python27 <silfer.lee@gmail.com>
2015-12-16 16:07:58 +0800

    modified rank computation tolerence

--- NetworkControllability/exact_controllability.py
+++ NetworkControllability/exact_controllability.py
@@ -1,3 +1,4 @@
+#coding=utf-8
 """
 exact controllability measure, driver nodes
 """
@@ -49,20 +50,32 @@ def get_number_of_driver_nodes(G):
     N = G.number_of_nodes()
     A = (nx.adjacency_matrix(G)).todense()  # get adjacency matrix A of G
     all_eigs = LA.eigvals(A)           # get eigenvalues of A
-    # get distince eigevalues of A, the precision is 1e-8, 
-    # which means any two eigenvalues less than 1e-8 will be considered as identical
-    L = np.unique(np.round(all_eigs,decimals=8))
+    # get distince eigevalues of A, the precision is 1e-5, 
+    # which means any two eigenvalues less than 1e-5 will be considered as identical
+    # L = np.unique(np.round(all_eigs,decimals=5))
+    L = list(set(np.round(all_eigs,8)))
+    #L = list(set(all_eigs))
 
     ND = -1
     ND_lambda = 0.0
+    IN = np.eye(N)
     for my_lambda in L:
         # get geometric multiplicity for each lambda of A
-        miu_lambda = N - LA.matrix_rank(my_lambda * np.eye(N) - A)  
+        miu_lambda = N - LA.matrix_rank(my_lambda * IN - A, tol=1E-6)
         if miu_lambda > ND:
             ND = miu_lambda
             ND_lambda = my_lambda
 
     return (ND, ND_lambda)
+
+def get_number_of_drivers_fast_rank(G):
+    """ Return the number of driver nodes using fast rank algorithm,
+        this algorithm can ONLY apply to large sparse networks
+    """
+    N = G.number_of_nodes()
+    A = (nx.adjacency_matrix(G)).todense()
+    myRank = np.linalg.matrix_rank(A, 1E-6);
+    return N - myRank;
 
 def get_driver_nodes(G):
     """Return the driver nodes number and driver nodes of a (directed or undirected) graph G
@@ -81,7 +94,7 @@ def get_driver_nodes(G):
     Returns:
     --------
     ND:              the number of driver nodes
-    ND_lambda:       the eigenvalue corresponding to maximum geometric multiplicity
+    driverNodes:     the list of driver nodes
 
     Note:
     -----
@@ -97,24 +110,21 @@ def get_driver_nodes(G):
     N = G.number_of_nodes()
     A = (nx.adjacency_matrix(G)).todense()          # get adjacency matrix A of G
     all_eigs = LA.eigvals(A)                        # get eigenvalues of A
-    # get distince eigevalues of A, the precision is 1e-8, 
     # which means any two eigenvalues less than 1e-8 will be considered as identical
-    L = np.unique(np.round(all_eigs,decimals=8))
+    L = list(set(np.round(all_eigs,8)))
 
     ND = -1
     ND_lambda = 0.0
+    IN = np.eye(N)
     for my_lambda in L:
         # get geometric multiplicity for each lambda of A
-        miu_lambda = N - LA.matrix_rank(my_lambda * np.eye(N) - A)  
+        miu_lambda = N - LA.matrix_rank(my_lambda * IN - A, tol=1E-8)
         if miu_lambda > ND:
             ND = miu_lambda
             ND_lambda = my_lambda
-    
-    # if ND_lambda is complex number, for simplicity, only keep its real part
-    if isinstance(ND_lambda, complex):
-        ND_lambda = ND_lambda.real
-    
+        
     middle_matrix = A - ND_lambda * np.eye(N)   # get the middle matrix A - \lambda * I_N
+    middle_matrix = np.round(middle_matrix, 8);
     (reduced_matrix, pivot_array) = sympy.Matrix(middle_matrix).rref()  # rref get the reduced row echelon form
     all_nodes = G.nodes() 
     pivot_nodes = [all_nodes[i] for i in pivot_array]
@@ -139,6 +149,7 @@ if __name__ == "__main__":
     G.add_edge(1, 1)
     G.add_edge(2, 2)  
     (ND, my_lambda) = get_number_of_driver_nodes(G)
+    print 'N = ', G.number_of_nodes();
     print 'ND = ', ND
     print 'my lambda = ', my_lambda
     (ND, pivot) = get_driver_nodes(G)
@@ -156,6 +167,7 @@ if __name__ == "__main__":
     G2.add_edge(3-1,4-1)
     (ND, my_lambda) = get_number_of_driver_nodes(G2)
     print '-'*20
+    print 'N = ', G2.number_of_nodes();
     print "ND:", ND;
     print "lambda:", my_lambda
     (ND, pivot) = get_driver_nodes(G2)
@@ -177,6 +189,7 @@ if __name__ == "__main__":
     G3.add_edge(1-1,4-1)
     (ND, my_lambda) = get_number_of_driver_nodes(G3)
     print '-'*20
+    print 'N = ', G3.number_of_nodes();
     print "ND:", ND;
     print "lambda:", my_lambda
     (ND, pivot) = get_driver_nodes(G3)
@@ -203,6 +216,7 @@ if __name__ == "__main__":
     G4.add_edge(2-1,6-1)
     (ND, my_lambda) = get_number_of_driver_nodes(G4)
     print '-'*20
+    print 'N = ', G4.number_of_nodes();
     print "ND:", ND;
     print "lambda:", my_lambda
     (ND, pivot) = get_driver_nodes(G4)
@@ -227,6 +241,7 @@ if __name__ == "__main__":
     G5.add_edge(4-1, 3-1)
     (ND, my_lambda) = get_number_of_driver_nodes(G5)
     print '-'*20
+    print 'N = ', G5.number_of_nodes();
     print "ND:", ND;
     print "lambda:", my_lambda
     (ND, pivot) = get_driver_nodes(G5)
@@ -246,6 +261,7 @@ if __name__ == "__main__":
     G6.add_edge(5, 4)
     (ND, my_lambda) = get_number_of_driver_nodes(G6)
     print '-'*20
+    print 'N = ', G6.number_of_nodes();
     print "ND:", ND;
     print "lambda:", my_lambda
     (ND, pivot) = get_driver_nodes(G6)
@@ -269,6 +285,7 @@ if __name__ == "__main__":
     G7.add_edges_from(edge_5)
     (ND, my_lambda) = get_number_of_driver_nodes(G7)
     print '-'*20
+    print 'N = ', G7.number_of_nodes();
     print "ND:", ND;
     print "lambda:", my_lambda
     (ND, pivot) = get_driver_nodes(G7)
@@ -282,6 +299,7 @@ if __name__ == "__main__":
     G8.add_edge(0, 5)
     (ND, my_lambda) = get_number_of_driver_nodes(G8)
     print '-'*20
+    print 'N = ', G8.number_of_nodes();
     print "ND:", ND;
     print "lambda:", my_lambda
     (ND, pivot) = get_driver_nodes(G8)
